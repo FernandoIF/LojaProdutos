b38e4fc6e628362e297170e6b190dce11ffbe451
FernandoIF <fernando.iftm@outlook.com>
2025-02-08 22:06:15 -0300

    Comunicação API ViaCep

--- obj/Debug/net8.0/LojaProdutos.AssemblyInfo.cs
+++ obj/Debug/net8.0/LojaProdutos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78e1e0302edf2e168634dbbb46655001a43ccb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f134c83c3d0aeea60a1767da6951a311239ac7b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
